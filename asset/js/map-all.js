dfb690add9c6dc855ed6764105fdc9a437600599
lee-han <t01062782363@gmail.com>
2024-06-26 00:01:27 +0900

    홈화면에는 데이터 10개씩 페이지네이션.

--- asset/js/map-all.js
+++ asset/js/map-all.js
@@ -1,79 +1,79 @@
 $(document).ready(function() {
-	var csvFilePath = "asset/csv/data.csv";
+    var csvFilePath = "asset/csv/data.csv";
 
-	function loadSpreadsheetData() {
-		fetch(csvFilePath)
-			.then(response => response.text())
-			.then(csvData => {
-				Papa.parse(csvData, {
-					header: true, // 첫 번째 행을 필드 이름으로 사용
-					skipEmptyLines: true, // 비어 있는 행 무시
-					dynamicTyping: true, // 숫자, 불리언 값은 자동으로 해당 타입으로 변환
-					complete: function(results) {
+    function loadSpreadsheetData() {
+        fetch(csvFilePath)
+            .then(response => response.text())
+            .then(csvData => {
+                Papa.parse(csvData, {
+                    header: true, // 첫 번째 행을 필드 이름으로 사용
+                    skipEmptyLines: true, // 비어 있는 행 무시
+                    dynamicTyping: true, // 숫자, 불리언 값은 자동으로 해당 타입으로 변환
+                    complete: function(results) {
 
-						$('#example').DataTable({
-							"dom": 'f<it>',
-							"data": results.data,
-							"columns": [
-								{data: '명칭'},
-								{data: '한자'},
-								{data: '위치'},
-								{data: '분류'},
-								{data: '이미지'},
-								{data: '기여자 기록용 별명 (한 번만 적어주시면 됩니다)', visible: false},
-							],
-							"lengthChange": false,
-							"paging": false,
-							"searching": true,
-							"info": true,
-							"language": {
-								"search": "검색 : ",
-								"searchPlaceholder": "어디를 찾으세요?",
-								"info": "총 _TOTAL_개. 원하는 항목을 클릭하세요.",
-								"infoEmpty": "0개의 항목",
-							},
-							"columnDefs": [
-								{
-									targets: 3,
-									render: function(data, type, row) {
-										var fillColor = getFillColorByCategory(data);
-										return `<span style="color: ${fillColor};">${data}</span>`;
-									}
-								}
-							],
-							"initComplete": function(settings, json) {
-								// DataTable 로딩 완료 후 클릭 이벤트 리스너 설정
-								$('div.dataTables_filter input').focus();
-								$('#example tbody').on('click', 'tr', function() {
-									var $this = $(this);
-									var nameData = $('td', this).eq(0).text();
-									var positionData = $('td', this).eq(2).text();
-									var categoryData = $("td", this).eq(3).text();
-									var imageData = $('td', this).eq(4).text(); // 이미지 데이터 가져오기
-									var positions = positionData.split(',');
-									var topPosition = positions[0].trim() + 'px';
-									var leftPosition = positions[1].trim() + 'px';
-									var leftPositionLegend = (parseInt(positions[1].trim()) + 50) + 'px';
-									var markerClass = 'marker-' + nameData.replace(/\s+/g, '-').toLowerCase();
-									
-									// 분류에 따라 색상 결정
-									var fillColor = getFillColorByCategory(categoryData);
-
-									// 이미지 데이터에 따라 페이지 이동
-									if (imageData === '수선전도') {
-										window.location.href = '/map1.html';
-									} else if (imageData === '동궐도') {
-										window.location.href = '/map2.html';
-									}
-								});
-							}
-						});
-					}
-				});
-			})
-			.catch(() => console.log("Can’t access response. Blocked by browser?"))
-	}
-	loadSpreadsheetData();
+                        $('#example').DataTable({
+                            "dom": 'f<it>',
+                            "data": results.data,
+                            "columns": [
+                                {data: '명칭'},
+                                {data: '한자'},
+                                {data: '위치'},
+                                {data: '분류'},
+                                {data: '이미지'},
+                                {data: '기여자 기록용 별명 (한 번만 적어주시면 됩니다)', visible: false},
+                            ],
+                            "lengthChange": false,
+                            "paging": true,
+                            "pageLength": 10, // 한 페이지에 보여줄 항목 수
+                            "searching": true,
+                            "info": true,
+                            "language": {
+                                "search": "검색 : ",
+                                "searchPlaceholder": "어디를 찾으세요?",
+                                "info": "총 _TOTAL_개. 원하는 항목을 클릭하세요.",
+                                "infoEmpty": "0개의 항목",
+                                "paginate": {
+                                    "first": "처음",
+                                    "last": "마지막",
+                                    "next": "다음",
+                                    "previous": "이전"
+                                }
+                            },
+                            "columnDefs": [
+                                {
+                                    targets: 3,
+                                    render: function(data, type, row) {
+                                        var fillColor = getFillColorByCategory(data);
+                                        return `<span style="color: ${fillColor};">${data}</span>`;
+                                    }
+                                }
+                            ],
+                            "initComplete": function(settings, json) {
+                                // DataTable 로딩 완료 후 클릭 이벤트 리스너 설정
+                                $('#example tbody').on('click', 'tr', function() {
+                                    var $this = $(this);
+                                    var nameData = $('td', this).eq(0).text();
+                                    var categoryData = $("td", this).eq(3).text();
+                                    var imageData = $('td', this).eq(4).text(); // 이미지 데이터 가져오기
+                                    
+                                    // 이미지 데이터에 따라 페이지 이동
+                                    if (imageData === '수선전도') {
+                                        window.location.href = '/map1.html';
+                                    } else if (imageData === '동궐도') {
+                                        window.location.href = '/map2.html';
+                                    }
+                                    
+                                    // 세션 스토리지에 데이터 저장
+                                    sessionStorage.setItem('selectedNaming', nameData);
+                                });
+                            }
+                        });
+                    }
+                });
+            })
+            .catch(() => console.log("Can’t access response. Blocked by browser?"))
+    }
+    loadSpreadsheetData();
 });
 
 // 위치 분류에 따라 색상 지정
@@ -91,10 +91,3 @@ function getFillColorByCategory(category) {
             return '#ffa500';
     }
 }
-
-// 테이블 행 클릭 이벤트 추가
-$('table tr').on('click', function() {
-    var naming = $(this).data('naming'); // 클릭한 행의 명칭 가져오기
-    sessionStorage.setItem('selectedNaming', naming); // 세션 스토리지에 저장
-    window.location.href = 'newPage.html'; // 새로운 페이지로 이동
-});
